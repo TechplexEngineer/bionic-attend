32407f49369a7630e2848778c8c558decb295f5f
Blake Bourque <Techplex.Engineer@gmail.com>
2021-10-09 15:35:16 -0400

    Add readme, remove unused seq code

---
+++ Readme.md
@@ -0,0 +1,26 @@
+Bionic Attendance
+=================
+
+Web application to track student attendance for FRC robotics team meetings.
+
+## How it works
+Users can be created by entering a userid that does not already exist in the system.
+Designed to be used with a barcode scanner which appends a "return" to the end of the scan.
+Does not track time, only if the student attended a meeting.
+
+## How we use it
+We have a raspberry pi with a keyboard, mouse, and barcode scanner near the entrance to our robotics shop.
+Students scan their student IDs when they enter the shop.
+
+## Technical
+Data is stored in a SQLite database in two tables, users and attendance.
+The database structure is specified in [schema.sql](db/schema.sql).
+THe queries are specified in [queries.sql](db/queries.sql).
+This project uses [sqlc](https://github.com/kyleconroy/sqlc) to generate go code which converts the query results into
+go datatypes. 
+
+### Dependencies
+- modernc.org/sqlite - pure go implementation of sqlite
+- github.com/gorilla/mux - for http routing
+- github.com/gorilla/sessions - for cross browser cookie creation and deletion
+- github.com/matryer/is - lightweight unit test helpers
--- helperTemplates.go
+++ helperTemplates.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"errors"
 	"fmt"
 	"github.com/gorilla/sessions"
 	"html/template"
@@ -9,8 +8,6 @@ import (
 	"log"
 	"net/http"
 	"time"
-
-	"github.com/spf13/cast"
 )
 
 const (
@@ -61,7 +58,6 @@ func LoadBaseTemplates(fs fs.FS, funcs *template.FuncMap) (*template.Template, e
 		"toJS": func(s string) template.JS {
 			return template.JS(s)
 		},
-		"seq": Seq,
 	})
 
 	if funcs != nil {
@@ -81,79 +77,4 @@ func LoadBaseTemplates(fs fs.FS, funcs *template.FuncMap) (*template.Template, e
 	}
 
 	return tmpl, nil
-}
-
-// Seq creates a sequence of integers.  It's named and used as GNU's seq.
-//
-// Examples:
-//     3 => 1, 2, 3
-//     1 2 4 => 1, 3
-//     -3 => -1, -2, -3
-//     1 4 => 1, 2, 3, 4
-//     1 -2 => 1, 0, -1, -2
-func Seq(args ...interface{}) ([]int, error) {
-	if len(args) < 1 || len(args) > 3 {
-		return nil, errors.New("invalid number of arguments to Seq")
-	}
-
-	intArgs := cast.ToIntSlice(args)
-	if len(intArgs) < 1 || len(intArgs) > 3 {
-		return nil, errors.New("invalid arguments to Seq")
-	}
-
-	inc := 1
-	var last int
-	first := intArgs[0]
-
-	if len(intArgs) == 1 {
-		last = first
-		if last == 0 {
-			return []int{}, nil
-		} else if last > 0 {
-			first = 1
-		} else {
-			first = -1
-			inc = -1
-		}
-	} else if len(intArgs) == 2 {
-		last = intArgs[1]
-		if last < first {
-			inc = -1
-		}
-	} else {
-		inc = intArgs[1]
-		last = intArgs[2]
-		if inc == 0 {
-			return nil, errors.New("'increment' must not be 0")
-		}
-		if first < last && inc < 0 {
-			return nil, errors.New("'increment' must be > 0")
-		}
-		if first > last && inc > 0 {
-			return nil, errors.New("'increment' must be < 0")
-		}
-	}
-
-	// sanity check
-	if last < -100000 {
-		return nil, errors.New("size of result exceeds limit")
-	}
-	size := ((last - first) / inc) + 1
-
-	// sanity check
-	if size <= 0 || size > 2000 {
-		return nil, errors.New("size of result exceeds limit")
-	}
-
-	seq := make([]int, size)
-	val := first
-	for i := 0; ; i++ {
-		seq[i] = val
-		val += inc
-		if (inc < 0 && val < last) || (inc > 0 && val > last) {
-			break
-		}
-	}
-
-	return seq, nil
 }
